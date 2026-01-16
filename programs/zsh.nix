f92e889e56342c2489a014d20d4d968b19bca73d
Dave Benvenuti <davebenvenuti@gmail.com>
2026-01-15 16:28:43 -0800

    We want this to be initExtra not initContent

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -20,7 +20,7 @@
     };
 
     # Read the extra configuration from a separate file
-    initContent = lib.mkDefault (builtins.readFile ./zsh.extra);
+    initExtra = lib.mkDefault (builtins.readFile ./zsh.extra);
   };
 
   # Further modified in features/zshrc-private-sync.nix
