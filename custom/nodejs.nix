ead23b7cd352eae9966f96990b32d0918dcafdfa
Dave Benvenuti <davebenvenuti@gmail.com>
2026-01-18 10:59:54 -0800

    We have to allow access to the internet to download an npm package

--- custom/nodejs.nix
+++ custom/nodejs.nix
@@ -4,13 +4,17 @@ with lib;
 
 let
   cfg = config.custom.nodejs;
-  
+
   # Create a derivation for each npm package
   # This builds a Nix derivation that runs `npm install --global` during build time.
   # Each package gets its own derivation for better caching - changing one package
   # only rebuilds that package, not all of them.
   mkGlobalNpmPackage = pkgName: pkgs.runCommand "npm-global-${builtins.replaceStrings ["@" "/"] ["_" "_"] pkgName}" {
     buildInputs = [ pkgs.nodejs pkgs.cacert ];
+
+    # Allow access to the internet
+    __noChroot = true;
+
     # Add SSL certificates for npm to work in the Nix sandbox
     SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
     NODE_EXTRA_CA_CERTS = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
@@ -19,23 +23,23 @@ let
     # Create the directory structure that npm expects for global installations
     mkdir -p $out/lib/node_modules
     mkdir -p $out/bin
-    
+
     # Tell npm to install to our custom output directory instead of the default
     # global location (which would be in the read-only Nix store)
     export npm_config_prefix=$out
-    
+
     # Set npm cache and tmp directories to writable locations
     # In the Nix build sandbox, /homeless-shelter is not writable
     export npm_config_cache=$TMPDIR/npm-cache
     export npm_config_tmp=$TMPDIR/npm-tmp
     mkdir -p $TMPDIR/npm-cache
     mkdir -p $TMPDIR/npm-tmp
-    
+
     # Install the package globally. This happens during Nix build time, not
     # during home-manager activation, making it more reproducible.
     npm install --global $pkgName
   '';
-  
+
   # Create all package derivations
   globalPackageDerivations = map mkGlobalNpmPackage cfg.globalNpmPackages;
 in {
