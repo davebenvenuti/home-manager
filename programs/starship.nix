216f6bc169537f28276d188af4e54593da8426e5
Dave Benvenuti <dave.benvenuti@shopify.com>
2026-02-23 19:30:40 -0800

    Display time info

--- programs/starship.nix
+++ programs/starship.nix
@@ -29,8 +29,30 @@
         disabled = false;
       };
 
+      cmd_duration = {
+        min_time = 0;
+        format = " duration: [$duration]($style)";
+        style = "bold yellow";
+      };
+
+      custom.cmd_start_time = {
+        description = "Command start time";
+        command = "echo $CMD_START_TIME";
+        when = "test -n \"$CMD_START_TIME\"";
+        format = "start: [$output]($style)";
+        style = "bold cyan";
+      };
+
+      custom.cmd_end_time = {
+        description = "Command end time";
+        command = "echo $CMD_END_TIME";
+        when = "test -n \"$CMD_END_TIME\"";
+        format = " end: [$output]($style)";
+        style = "bold green";
+      };
+
       # https://calvinpyong.com/blog/starship-robbyrussell/
-      format = "$custom$character$directory$git_branch$git_status";
+      format = "$custom$character$directory$git_branch$git_status\n\${custom.cmd_start_time}\${custom.cmd_end_time}$cmd_duration";
 
       hostname = {
         ssh_only = false;
