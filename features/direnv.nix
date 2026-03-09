260224d9823a8534529dd0ed0d41f4b482a86b22
Dave Benvenuti <dave.benvenuti@shopify.com>
2026-03-09 10:40:00 -0700

    Enable zsh integration in direnv

--- features/direnv.nix
+++ features/direnv.nix
@@ -3,7 +3,9 @@
   programs.direnv = lib.mkIf features.direnv {
     enable = true;
     nix-direnv.enable = true;
-    
+
+    enableZshIntegration = true;
+
     config = {
       global = {
         hide_env_diff = true;
