f991b78f3765eaa8c7a5648bc2db6a1df2823b74
Dave Benvenuti <davebenvenuti@gmail.com>
2026-03-14 08:42:48 -0700

    Add tree

--- home.nix
+++ home.nix
@@ -51,6 +51,8 @@ in
     jq
 
     ripgrep
+
+    tree
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
