226511b71fac4b46caeb85e46d6552f1df345b64
Dave Benvenuti <dave.benvenuti@shopify.com>
2026-03-17 13:03:54 -0700

    iotop isn't available on darwin

--- features/monitoring.nix
+++ features/monitoring.nix
@@ -1,13 +1,16 @@
 { lib, features, pkgs, ... }:
 {
-  home.packages = with pkgs; lib.mkIf features.monitoring [
-    htop
-    btop
-    procs
-    duf
-    ncdu
-    iotop
-    bandwhich
-    nmon
-  ];
+  home.packages = with pkgs; lib.mkIf features.monitoring (
+    [
+      htop
+      btop
+      procs
+      duf
+      ncdu
+      bandwhich
+      nmon
+    ] ++ lib.optionals (!stdenv.isDarwin) [
+      iotop
+    ]
+  );
 }
